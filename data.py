4d7b3d481ff127f83f4e72d1911bb44e1c81f5bb
Maxat Kulmanov <coolmaksat@gmail.com>
2021-07-05 15:24:38 +0300

    data files

---
+++ data.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+import click as ck
+import numpy as np
+import pandas as pd
+import gzip
+import logging
+import torch as th
+
+import jpype
+import jpype.imports
+import os
+
+jars_dir = "jars/"
+jars = f'{str.join(":", [jars_dir + name for name in os.listdir(jars_dir)])}'
+
+if not jpype.isJVMStarted():
+    jpype.startJVM(
+        jpype.getDefaultJVMPath(), "-ea",
+        "-Djava.class.path=" + jars,
+        convertStrings=False)
+
+
+# OWLAPI imports
+from org.semanticweb.owlapi.model import OWLOntology
+from org.semanticweb.owlapi.apibinding import OWLManager
+from org.semanticweb.owlapi.reasoner import ConsoleProgressMonitor
+from org.semanticweb.owlapi.reasoner import SimpleConfiguration
+from org.semanticweb.owlapi.reasoner import InferenceType
+from org.semanticweb.owlapi.util import InferredClassAssertionAxiomGenerator
+from org.semanticweb.owlapi.util import InferredEquivalentClassAxiomGenerator
+from org.semanticweb.owlapi.util import InferredSubClassAxiomGenerator
+from de.tudresden.inf.lat.jcel.owlapi.main import JcelReasoner
+from de.tudresden.inf.lat.jcel.ontology.normalization import OntologyNormalizer
+from de.tudresden.inf.lat.jcel.ontology.axiom.extension import IntegerOntologyObjectFactoryImpl
+from de.tudresden.inf.lat.jcel.owlapi.translator import ReverseAxiomTranslator
+from org.semanticweb.owlapi.model import OWLAxiom
+from java.util import HashSet
+from java.io import File
+
+
+
+
+@ck.command()
+def main():
+    ont_manager = OWLManager.createOWLOntologyManager()
+    data_factory = ont_manager.getOWLDataFactory()
+    ontology = ont_manager.loadOntologyFromOntologyDocument(
+            File("data/go.owl"))
+    jReasoner = JcelReasoner(ontology, False)
+    rootOnt = jReasoner.getRootOntology()
+    translator = jReasoner.getTranslator()
+    axioms = HashSet()
+    axioms.addAll(rootOnt.getAxioms())
+    translator.getTranslationRepository().addAxiomEntities(
+        rootOnt)
+    for ont in rootOnt.getImportsClosure():
+        axioms.addAll(ont.getAxioms())
+        translator.getTranslationRepository().addAxiomEntities(
+            ont)
+
+    intAxioms = translator.translateSA(axioms)
+
+    normalizer = OntologyNormalizer()
+    factory = IntegerOntologyObjectFactoryImpl()
+    normalizedOntology = normalizer.normalize(intAxioms, factory)
+    rTranslator = ReverseAxiomTranslator(translator, ontology)
+
+    gos_df = pd.read_pickle('data/nodes.pkl')
+    terms = gos_df['nodes'].values
+    terms_dict = {v:k for k, v in enumerate(terms)}
+    relations = []
+    rel_id = {}
+    subclass = []
+    gos = {}
+    prefix = '<http://purl.obolibrary.org/obo/GO_'
+    lp = len(prefix)
+    for ax in normalizedOntology:
+        try:
+            axiom = f'{rTranslator.visit(ax)}'[11:-1]
+            it = axiom.split(' ')
+            if len(it) == 2:
+                c1 = to_go(it[0])
+                c2 = to_go(it[1])
+                if c1 in terms_dict and c2 in terms_dict:
+                    c1 = terms_dict[c1]
+                    c2 = terms_dict[c2]
+                    subclass.append((c1, c2))
+            elif len(it) == 3 and it[1].startswith('ObjectSomeValuesFrom('):
+                c1 = to_go(it[0])
+                rel = to_rel(it[1])
+                c2 = to_go(it[2][:-1])
+                if c1 in terms_dict and c2 in terms_dict:
+                    c1 = terms_dict[c1]
+                    c2 = terms_dict[c2]
+                    if rel not in rel_id:
+                        rel_id[rel] = len(rel_id)
+                    relations.append((c1, rel_id[rel], c2))
+        except Exception as e:
+            print(f'Ignoring {ax}', e)
+
+    rel_id['hasFunction'] = len(rel_id)
+    df = pd.read_pickle('data/train_data.pkl')
+    hasfunc = []
+    for i, row in enumerate(df.itertuples()):
+        p_id = row.proteins
+        for go_id in row.exp_annotations:
+            go_id = terms_dict[go_id]
+            hasfunc.append((p_id, go_id))
+    sub_df = pd.DataFrame({'subclass': subclass})
+    rel_df = pd.DataFrame({'relations': relations})
+    hf_df = pd.DataFrame({'hasfunc': hasfunc})
+    sub_df.to_pickle('data/train_subclass.pkl')
+    rel_df.to_pickle('data/train_relations.pkl')
+    hf_df.to_pickle('data/train_hasfunc.pkl')
+
+    rel_id_df = pd.DataFrame({'relations': list(rel_id.keys()), 'ids': list(rel_id.values())})
+    rel_id_df.to_pickle('data/relations.pkl')
+    
+    
+
+
+def to_go(uri):
+    return uri[1:-1].replace('http://purl.obolibrary.org/obo/GO_', 'GO:')
+
+def to_rel(uri):
+    return uri[len('ObjectSomeValuesFrom(<http://purl.obolibrary.org/obo/'):-1]
+
+if __name__ == '__main__':
+    main()
---
+++ deepgo_data.py
@@ -0,0 +1,80 @@
+import click as ck
+import numpy as np
+import pandas as pd
+from collections import Counter
+from utils import Ontology, FUNC_DICT
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+@ck.command()
+@ck.option(
+    '--go-file', '-gf', default='data/go.obo',
+    help='Gene Ontology file in OBO Format')
+@ck.option(
+    '--data-file', '-ndf', default='data/swissprot.pkl',
+    help='Uniprot KB, generated with uni2pandas.py')
+@ck.option(
+    '--out-nodes-file', '-onf', default='data/nodes.pkl',
+    help='')
+@ck.option(
+    '--train-data-file', '-trdf', default='data/train_data.pkl',
+    help='Result file with a list of terms for prediction task')
+@ck.option(
+    '--test-data-file', '-tsdf', default='data/test_data.pkl',
+    help='Result file with a list of terms for prediction task')
+def main(go_file, data_file,
+         out_nodes_file, train_data_file, test_data_file):
+    go = Ontology(go_file, with_rels=True)
+    logging.info('GO loaded')
+
+    df = pd.read_pickle(data_file)
+    print("DATA FILE" ,len(df))
+    
+    logging.info('Processing annotations')
+    
+    cnt = Counter()
+    annotations = list()
+    for i, row in df.iterrows():
+        for term in row['prop_annotations']:
+            cnt[term] += 1
+    
+    # # Filter terms with annotations more than min_count
+    # res = {}
+    # for key, val in cnt.items():
+    #     if val >= min_count:
+    #         ont = key.split(':')[0]
+    #         if ont not in res:
+    #             res[ont] = []
+    #         res[ont].append(key)
+    # terms = []
+    # for key, val in res.items():
+    #     print(key, len(val))
+    #     terms += val
+
+    terms = list(cnt.keys())
+
+    logging.info(f'Number of terms {len(terms)}')
+    
+    # Save the list of terms
+    terms_df = pd.DataFrame({'nodes': terms})
+    terms_df.to_pickle(out_nodes_file)
+
+    n = len(df)
+    # Split train/valid
+    index = np.arange(n)
+    train_n = int(n * 0.95)
+    np.random.seed(seed=0)
+    np.random.shuffle(index)
+    train_df = df.iloc[index[:train_n]]
+    test_df = df.iloc[index[train_n:]]
+
+    print('Number of train proteins', len(train_df))
+    train_df.to_pickle(train_data_file)
+
+    print('Number of test proteins', len(test_df))
+    test_df.to_pickle(test_data_file)
+
+
+if __name__ == '__main__':
+    main()
